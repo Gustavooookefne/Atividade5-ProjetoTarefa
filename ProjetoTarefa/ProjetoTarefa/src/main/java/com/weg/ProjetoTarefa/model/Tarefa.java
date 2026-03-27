634e72fb17150b9d54fef6913eb337d64ea31404
Gustavooookefne <gustavooookefne14@gmail.com>
2026-03-27 19:02:23 -0300

    feat:criado Tarefa model

--- ProjetoTarefa/ProjetoTarefa/src/main/java/com/weg/ProjetoTarefa/model/Tarefa.java
+++ ProjetoTarefa/ProjetoTarefa/src/main/java/com/weg/ProjetoTarefa/model/Tarefa.java
@@ -1,4 +1,26 @@
 package com.weg.ProjetoTarefa.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Tarefa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome_tarefa" , nullable = false)
+    private String nomeTarefa;
+
+    @Column(name = "tipo" , nullable = false)
+    private String tipo;
+
+    @Column(name = "quantidade" , nullable = false)
+    private Integer quantidade;
 }
