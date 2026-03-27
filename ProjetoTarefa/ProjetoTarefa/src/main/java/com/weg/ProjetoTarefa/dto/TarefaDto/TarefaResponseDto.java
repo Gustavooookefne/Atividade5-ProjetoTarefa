691bbb3ea72c36bcd0a6d2a033d1f27a8eabe909
Gustavooookefne <gustavooookefne14@gmail.com>
2026-03-27 19:08:33 -0300

    feat:criada a class TarefaResponseDto

--- ProjetoTarefa/ProjetoTarefa/src/main/java/com/weg/ProjetoTarefa/dto/TarefaDto/TarefaResponseDto.java
+++ ProjetoTarefa/ProjetoTarefa/src/main/java/com/weg/ProjetoTarefa/dto/TarefaDto/TarefaResponseDto.java
@@ -1,4 +1,9 @@
 package com.weg.ProjetoTarefa.dto.TarefaDto;
 
-public class TarefaResponseDto {
+public record TarefaResponseDto (
+        Long id,
+        String nomeTarefa,
+        String tipo,
+        Integer quantidade
+){
 }
