78d7990363307d8bdda5be684245550be642cf34
Gustavooookefne <gustavooookefne14@gmail.com>
2026-03-27 19:00:24 -0300

    feat: Criando class e criado Projeto model

---
+++ ProjetoTarefa/ProjetoTarefa/src/main/java/com/weg/ProjetoTarefa/model/Projeto.java
@@ -0,0 +1,29 @@
+package com.weg.ProjetoTarefa.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Projeto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "nome_projeto", nullable = false)
+    private String nomeProjeto;
+
+    @Column(name = "descriçao", nullable = false)
+    private String descricao;
+
+    @Column(name = "duraçao", nullable = false)
+    private LocalDate duracao;
+}
---
+++ ProjetoTarefa/ProjetoTarefa/src/main/java/com/weg/ProjetoTarefa/model/Tarefa.java
@@ -0,0 +1,4 @@
+package com.weg.ProjetoTarefa.model;
+
+public class Tarefa {
+}
