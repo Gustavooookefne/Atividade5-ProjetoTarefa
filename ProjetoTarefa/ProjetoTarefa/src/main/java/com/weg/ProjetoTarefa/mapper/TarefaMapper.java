b317cc38a39612b48fb07e6ec7ccb06e9a81e207
Gustavooookefne <gustavooookefne14@gmail.com>
2026-03-27 19:24:50 -0300

    refactor: Arrumando erros

--- ProjetoTarefa/ProjetoTarefa/src/main/java/com/weg/ProjetoTarefa/mapper/TarefaMapper.java
+++ ProjetoTarefa/ProjetoTarefa/src/main/java/com/weg/ProjetoTarefa/mapper/TarefaMapper.java
@@ -1,4 +1,25 @@
 package com.weg.ProjetoTarefa.mapper;
 
+import com.weg.ProjetoTarefa.dto.TarefaDto.TarefaRequestDto;
+import com.weg.ProjetoTarefa.dto.TarefaDto.TarefaResponseDto;
+import com.weg.ProjetoTarefa.model.Tarefa;
+
 public class TarefaMapper {
+
+    public Tarefa paraEntidade (TarefaRequestDto requestDto) {
+        return new Tarefa(
+                requestDto.nomeTarefa(),
+                requestDto.tipo(),
+                requestDto.quantidade()
+        );
+    }
+
+    public TarefaResponseDto paraDto (Tarefa tarefa) {
+        return new TarefaResponseDto(
+                tarefa.getId(),
+                tarefa.getNomeTarefa(),
+                tarefa.getTipo(),
+                tarefa.getQuantidade()
+        );
+    }
 }
